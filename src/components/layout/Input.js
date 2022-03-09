4632c8e8fbe939d5b1c4257f11261cc0b01eedb3
Douglas Pereira <dougls99@outlook.com>
2022-03-09 19:59:32 -0300

    realizando ajustes

--- src/components/layout/Input.js
+++ src/components/layout/Input.js
@@ -1,31 +1,24 @@
 import styles from "./Input.module.css";
-import { useState } from "react";
 import search from "../img/search.svg";
-import { ErrorBoundary } from "react-error-boundary";
-
 
 function Input() {
-  <ErrorBoundary
   
-
-
   return (
 
     <input
-     className={styles.input} 
-     onChange={handleChange}
+     className={styles.input}
      placeholder="Insira aqui o nome da cidade" 
-     value={city}>
+     value={city}
+     >
     
-     <img 
+    <img
      className={styles.search} 
      src={search} 
      alt="search"
-     onClick={handleSearch}>
-     </img>
-    </input>>
+    />
+    
+
   )
-  </ErrorBoundary>
 }
 
 export default Input;
