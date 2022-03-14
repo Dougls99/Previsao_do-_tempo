b187ac765d1fd0ea9ab8629e7ce1174427ee6160
Douglas Pereira <dougls99@outlook.com>
2022-03-14 19:26:13 -0300

    Realizando Ajustes e Correção de Erros

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,13 +1,38 @@
+import { useState } from "react";
 import styles from "./Home.module.css";
-import Input from "./Input";
-
+import search from "../img/search.svg";
 
 function Home() {
+
+  const [city, setCity] = useState("")
+
+  const handleChange = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handleSearch = () => {
+    fetch()
+  }
+
   return (
 
   <header className={styles.header}>
     <h1>Previsão do tempo</h1>
-    <Input />
+    <div className="Previsao">
+    </div>
+    <div>
+    <input
+     className={styles.input}
+     placeholder="Insira aqui o nome da cidade"
+     onChange={handleChange}
+     value={city}
+    /> 
+    <img
+     className={styles.search} 
+     src={search} 
+     alt="search"
+    />
+    </div>
     <hr></hr>
     <h2>Capitais</h2>
   </header>
