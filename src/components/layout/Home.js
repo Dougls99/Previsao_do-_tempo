920ee7deec733178721b9f39e5f35d7e0dafa9a5
Douglas Pereira <dougls99@outlook.com>
2022-04-11 21:11:20 -0300

    centralizando div da previsao

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -3,7 +3,7 @@ import styles from './Home.module.css'
 import search from '../img/search.svg'
 
 function Home() {
-  const [climas, setClimas] = useState("")
+  const [climas, setClimas] = useState('')
 
   const [city, setCity] = useState('')
 
@@ -22,38 +22,42 @@ function Home() {
 
   return (
     <header className={styles.header}>
+      <h1>Previsão do tempo </h1>
 
-        <h1>Previsão do tempo </h1>
-    
       <div className={styles.input}>
         <input
           placeholder="Insira aqui o nome da cidade"
           onChange={handleChange}
           value={city}
         />
-        <img 
-          src={search}
-          alt="search"
-          onClick={handleSearch}
-        />
+        <img src={search} alt="search" onClick={handleSearch} />
       </div>
 
       {climas ? (
         <div className={styles.previsao}>
-          <h3>{climas.name} {climas.sys.country}</h3>
-          <p>{climas.main.temp}ºC {climas.weather[0].description}</p> 
-          <p>min {climas.main.temp_min}º  máx {climas.main.temp_max}º</p>
+          <h3>
+            {climas.name} {climas.sys.country}
+          </h3>
+          <p>
+            {climas.main.temp}ºC {climas.weather[0].description}
+          </p>
+          <p>
+            min {climas.main.temp_min}º máx {climas.main.temp_max}º
+          </p>
           <p>Sensação {climas.main.feels_like}</p>
           <p>Humidade {climas.main.humidity}%</p>
           <p>Vento: {climas.wind.speed}km/h</p>
-
         </div>
       ) : (
         <div></div>
       )}
       <hr></hr>
       <h2>Capitais</h2>
-      
+      <div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
     </header>
   )
 }
